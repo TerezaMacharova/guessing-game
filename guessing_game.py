2667752284362114ef8fc2205efe9a31e82447b1
macharot <macharovaterezia@gmail.com>
2024-02-15 18:42:17 +0100

    another game option added

--- guessing_game.py
+++ guessing_game.py
@@ -1,4 +1,5 @@
 import json
+from spellchecker import SpellChecker
 
 filename='animaltree.json'
 
@@ -13,7 +14,12 @@ def load_tree(filename):
         with open(filename, 'r') as file:
             return json.load(file)
     except FileNotFoundError:
-        return (print("File was not found."))
+     #   return (print("File was not found."))
+        return { #so the gaem can start with the default
+            "question": "Does it havr feathers?",
+            "yes": "parrot",
+            "no": "cat"
+            }
 
 
 def yes_no_question(question):
@@ -42,8 +48,29 @@ def guess_animal(node, parent=None, last_answer=None):
             return guess_animal(node['no'], node, 'no')
 
 
+
+def get_spell_checked_input(prompt):
+    spell = SpellChecker()
+
+    while True:
+        user_input = input(prompt).strip().capitalize()
+        # Check if the word is misspelled
+        misspelled = spell.unknown([user_input])
+        if misspelled:
+            # Suggest the most likely correction
+            suggestion = spell.correction(user_input)
+            confirmation = input(f"Did you mean '{suggestion}'? (yes/no): ").strip().lower()
+            if confirmation == 'yes':
+                return suggestion
+            else:
+                print("Please enter the animal name again.")
+        else:
+            # If the word is not misspelled, return it as is
+            return user_input
+
+
 def new_animal(parent, last_answer, guess):
-    new_animal = input("I give up. What was your animal? ").strip().lower()
+    new_animal = get_spell_checked_input("I give up. What was your animal? ").strip().lower()
 
     question = input(f"Please enter a question that distinguishes a {new_animal} from a {guess}: ").strip()
     question = question[0].upper() + question[1:].lower()
@@ -61,38 +88,40 @@ def new_animal(parent, last_answer, guess):
 
 
 def update_tree(parent, last_answer, guess, new_animal, question, answer_new_animal):
-        
-    #if last_answer:
-        new_node = {
-            "question": question,
-            "yes": new_animal if answer_new_animal == 'yes' else guess,
-            "no": guess if answer_new_animal == 'yes'else new_animal,
+    new_node = {        
+       "question": question,
+        "yes": new_animal if answer_new_animal == 'yes' else guess,
+        "no": guess if answer_new_animal == 'yes'else new_animal,
         }
-        if last_answer == 'yes':
-            parent['yes'] = new_node
-        else:
-            parent['no'] = new_node
-    #else:
-    #    # This would be for the root node if necessary
-    #    tree['question'] = question
-    #    tree['yes'] = new_animal if last_answer == 'yes' else guess
-    #    tree['no'] = guess if last_answer == 'yes' else new_animal
 
+    if last_answer == 'yes':
+        parent['yes'] = new_node
+    else:
+        parent['no'] = new_node
+    
 
 def play():
-    tree = load_tree(filename)
-    if tree is None:
-        tree = {
-            "question": "Does it fly?",
-            "yes": "bird",
-            "no": "fish",
-        }
+    while True:
+        tree = load_tree(filename)
 
-    path_info = guess_animal(tree)
-    if path_info:  # If the guess was incorrect
-        parent, last_answer, guess = path_info
-        new_animal(parent, last_answer, guess)
-        save_tree(tree, filename)
-        print("Thank you! I've learned something new.")
+        if tree is None:
+            tree = {
+                "question": "Does it fly?",
+                "yes": "bird",
+                "no": "fish",
+            }
+
+        path_info = guess_animal(tree)
+        if path_info:  # If the guess was incorrect
+            parent, last_answer, guess = path_info
+            new_animal(parent, last_answer, guess)
+            save_tree(tree, filename)
+            print("Thank you! I've learned something new.")
+
+        play_again = yes_no_question("Do you wanna play again?")
+        if play_again == 'no':
+            print("Thanks for playing! See you next time.")
+            break
+            
 
 play()
