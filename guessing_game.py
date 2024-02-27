211ebaa172a089c661c86ab5461c5d0fe6839a90
macharot <macharovaterezia@gmail.com>
2024-02-27 14:26:00 +0100

    fixing the indentation error

--- guessing_game.py
+++ guessing_game.py
@@ -1,20 +1,22 @@
 import json
-from tkinter import LAST
 from spellchecker import SpellChecker
-#import  tkinter as tk
-#from tkinter import messagebox
+
 
 filename = 'animaltree.json'
 
+
 class GuessingGame:
+    """guessing game where the computer guesses animals baseed on yes/no questions """
+
     def __init__(self, filename):
+        """ initializing the game by loading the decision tree """
         self.filename = filename
         self.tree = self.load_tree()
         self.current_node = None
 
 
     def save_tree(self):
-        """ save the tree to a file """
+        """ save the current tree to a file """
         with open(self.filename, 'w') as file:
             json.dump(self.tree, file, indent=4)
 
@@ -25,15 +27,17 @@ class GuessingGame:
             with open(self.filename, 'r') as file:
                 return json.load(file)
         except FileNotFoundError:
-            return { #so the game can start with the default
-                "question": "Does it havr feathers?",
+            return { 
+                "question": "Does it have feathers?",
                 "yes": "parrot",
-                "no": "cat"
+                "no": "cat",
                 }
 
 
     def yes_no_question(self, question):
-        while True: 
+        """ asks the user yes/no question
+        continues until the answer is valid """
+        while True:
             answer = input(question + "(yes/no):" ).strip().lower()
             if answer == 'yes' or answer == 'no':
                 return answer
@@ -42,13 +46,15 @@ class GuessingGame:
 
 
     def guess_animal(self, node, parent=None, last_answer=None):
+        """ guesses the animal by going throught the tree
+        if the guess is incorrect it lets the user teach nee animal """
         if isinstance(node, str):
             guess = node
             user_answer = self.yes_no_question(f"Is your animal {guess}?")
             if user_answer == "yes":
                 print("I won, you lost! Haha!")
                 return True
-            else: 
+            else:
                 if parent is not None:
                     self.new_animal(parent, last_answer, guess)
                 return False
@@ -63,11 +69,11 @@ class GuessingGame:
 
 
     def spell_check(self, prompt):
+        """ spell checking the user input """
         spell = SpellChecker()
 
         while True:
             user_input = input(prompt).strip().capitalize()
-            # Check if the word is misspelled
             misspelled = spell.unknown([user_input])
             if misspelled:
                 # Suggest the most likely correction
@@ -78,11 +84,11 @@ class GuessingGame:
                 else:
                     print("Please enter the animal name again.")
             else:
-                # If the word is not misspelled, return it as is
                 return user_input
 
 
     def new_animal(self, parent, last_answer, guess):
+        """ teaching the game a new animal from the user """
         new_animal = self.spell_check("I give up. What was your animal? ").strip().lower()
 
         question = input(f"Please enter a question that distinguishes a {new_animal} from a {guess}: ").strip()
@@ -102,8 +108,9 @@ class GuessingGame:
 
 
     def update_tree(self, parent, last_answer, guess, new_animal, question, answer_new_animal):
-        new_node = {        
-           "question": question,
+        """ updating the tree with a branch for the new animal """
+        new_node = {
+            "question": question,
             "yes": new_animal if answer_new_animal == 'yes' else guess,
             "no": guess if answer_new_animal == 'yes'else new_animal,
             }
@@ -112,21 +119,22 @@ class GuessingGame:
             parent['yes'] = new_node
         else:
             parent['no'] = new_node
-    
+
 
     def play(self):
-         while True:
+        """ starting the game loop, allowing the user to play multiple rounds """
+        while True:
 
             self.current_node = self.tree  # Reset the current node to the start of the tree
             guessed_correctly = self.guess_animal(self.current_node)
             if not guessed_correctly:
-                # If the game did not guess correctly, it already went through learning new animal.
-                print("Ready for a new round or learn a new animal.")
+                print("Ready for a new round or learning a new animal.")
 
             play_again = self.yes_no_question("Do you want to play again?")
             if play_again == 'no':
                 print("Thanks for playing! See you next time.")
                 break
+
 
 print("Hello! Let's start the game!")
 if __name__ == "__main__":
